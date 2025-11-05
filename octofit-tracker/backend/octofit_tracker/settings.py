24ac3750993071167a332abb31d480d7578de2c1
alvisebonivento77 <alvisebonivento77@gmail.com>
2025-11-05 19:06:40 +0000

    Aggiorna settings.py e urls.py per supporto Codespace REST API dinamico

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-0qx&r2mh&ohn*@6hx#3via$e$j@tn-w%yy0y(!h^$yeg9#$03#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
@@ -86,7 +90,6 @@ DATABASES = {
             'username': '',
             'password': '',
             'authSource': 'admin',
-            'authMechanism': '',
         },
     }
 }
